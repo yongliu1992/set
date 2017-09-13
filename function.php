3fd5afbe543b93d36c1ef8dc511073851ae9d3a4
yongliu <a3831524@126.com>
2017-09-13 23:54:44 +0800

    aaa

--- function.php
+++ function.php
@@ -10,3 +10,52 @@ function writeLog($path,$text) {
     if(is_array($text))$text = var_export($text,ture);
     file_put_contents ( $path, date ( "Y-m-d H:i:s" ) . "  " . $text . "\r\n", FILE_APPEND );
 }
+
+function formatNum($number){
+    $number=substr($number,0,2);
+    $arr=array("零","一","二","三","四","五","六","七","八","九");
+    if(strlen($number)==1){
+        $result=$arr[$number];
+    }else{
+        if($number==10){
+            $result="十";
+        }else{
+            if($number<20){
+                $result="十";
+            }else{
+                $result=$arr[substr($number,0,1)]."十";
+            }
+            if(substr($number,1,1)!="0"){
+                $result.=$arr[substr($number,1,1)];
+            }
+        }
+    }
+    return $result;
+}
+//去除数组中重复的值
+ function unique_arr($array2D,$stkeep=false,$ndformat=true)
+{
+    // 判断是否保留一级数组键 (一级数组键可以为非数字)
+    if($stkeep) $stArr = array_keys($array2D);
+    // 判断是否保留二级数组键 (所有二级数组键必须相同)
+    if($ndformat) $ndArr = array_keys(end($array2D));
+    //降维,也可以用implode,将一维数组转换为用逗号连接的字符串
+    foreach ($array2D as $v){
+        $v = join(",",$v);
+        $temp[] = $v;
+    }
+    //去掉重复的字符串,也就是重复的一维数组
+    $temp = array_unique($temp);
+    //再将拆开的数组重新组装
+    foreach ($temp as $k => $v)
+    {
+        if($stkeep) $k = $stArr[$k];
+        if($ndformat)
+        {
+            $tempArr = explode(",",$v);
+            foreach($tempArr as $ndkey => $ndval) $output[$k][$ndArr[$ndkey]] = $ndval;
+        }
+        else $output[$k] = explode(",",$v);
+    }
+    return $output;
+}
