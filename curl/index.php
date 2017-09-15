16ec24db19faa2f3fdd8c971a93e8f5af34ed45c
yongliu <a3831524@126.com>
2017-09-15 14:57:59 +0800

    编程

--- curl/index.php
+++ curl/index.php
@@ -6,32 +6,14 @@
  * Time: 07:33
  */
 
-$tmp =array (
-    'payment_type' => '1',
-    'trade_no' => '2017090721001004020259788723',
-    'subject' => '很美丽',
-    'buyer_email' => 'a38***@126.com',
-    'gmt_create' => '2017-09-07 16:21:53',
-    'notify_type' => 'trade_status_sync',
-    'quantity' => '1',
-    'out_trade_no' => '20170907162140997922',
-    'seller_id' => '2088021970397651',
-    'notify_time' => '2017-09-07 16:21:54',
-    'trade_status' => 'TRADE_SUCCESS',
-    'is_total_fee_adjust' => 'N',
-    'total_fee' => '0.01',
-    'gmt_payment' => '2017-09-07 16:21:54',
-    'seller_email' => 'marcsong@zggonglue.com',
-    'price' => '0.01',
-    'buyer_id' => '2088102328382023',
-    'notify_id' => '5ca75d65013dca0b4dbea057a288c09g5m',
-    'use_coupon' => 'N',
-    'sign_type' => 'MD5',
-    'sign' => '05d5b0ac55ba3d5216b526986ad279cc',
-);
+$tmp =
+    'courseId=129&sign=0b5dadc76bbcfb48e70ff8c14b87f12c&timestamp=1505403841857.143799&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MDUzMTg1OTMsImp0aSI6ImNsYXNzLWNuLXRva2VuLWtvYWxhIiwiaXNzIjoiY2xhc3MuY24iLCJkYXRhIjp7InVzZXJJZCI6IjY4MTY5IiwibW9iaWxlIjoiMTgwMDAwMDAwMDAiLCJsb2dpblRva2VuIjoiYzQzNTJiOWEyZTkwZjczNmE4ZmVhYmY4NDc0MTU3NzgiLCJlZGl0aW9uSWQiOiIxIiwiZGV2aWNlVG9rZW4iOiIwNzAwM2MwN2ViMWY4OWI3YmI0MzFlYTAxNWM1MTM4MDA3MDAzYzA3ZWIxZjg5YjdiYjQzMWVhMDE1YzUxMzgwIn19.5aUPrY4NFddUCt9vJVk0YMHtKEcdw9ICFEqS0Fe35vw';
 
-$login_url = 'http://php.shaoziketang.com/user/login';
-$login_url = 'http://php.shaoziketang.com/callback/ali';
+
+
+
+$login_url = 'http://http://api.koalapass.com/course/getChapter?'.$tmp;
+var_dump(file_get_contents($tmp));exit;
 $ch = curl_init($login_url);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -39,14 +21,12 @@ curl_setopt($ch, CURLOPT_MAXREDIRS, 1);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
 curl_setopt($ch, CURLOPT_POST, 1);
-$mobile = 13965019845;
-$post_fields = [
-    'pwd'=>'123456',
-    'mobile'=>17600736503,
-    'src'=>4
-];
+
+//parse_str($tmp,$post_fields);
+//$post_fields = $tmp;
+//$post_fields = http_build_query($post_fields);
+//var_export($post_fields);exit;
 $post_fields = $tmp;
-$post_fields = http_build_query($post_fields);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
 curl_setopt($ch, CURLOPT_COOKIESESSION, true);
 
